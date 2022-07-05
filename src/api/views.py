c716f4ba100f59ef7a0b33033cfca4f219e2211c
Zafarbek Kamuljonov <z.kamuljonov@gmail.com>
2022-07-05 09:09:41 +0500

    basic configurations and product model

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from src.api.serializers import ProductSerializer
+from src.core.models import Product
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (permissions.AllowAny, )
+    http_method_names = ('get', 'post', 'put', 'delete')
